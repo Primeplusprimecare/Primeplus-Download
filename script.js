9dd7ac27da63197c5b09d7d5a208a3736d692ee4
Primeplusprimecare <support@primecareclinic.co.id>
2026-07-16 10:33:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-const PLAYSTORE =
-"https://play.google.com/store/apps/details?id=id.primecare.primeplus";
+document.addEventListener("DOMContentLoaded", () => {
+    const PLAYSTORE = "https://play.google.com/store/apps/details?id=id.primecare.primeplus";
+    const APPSTORE = "https://apps.apple.com/id/app/primeplus/id6785361573";
 
-const APPSTORE = 
-  "https://apps.apple.com/id/app/primeplus/id6785361573";
+    const playBtn = document.getElementById("playBtn");
+    const iosBtn = document.getElementById("iosBtn");
 
-const playBtn=document.getElementById("playBtn");
-const iosBtn=document.getElementById("iosBtn");
-const info=document.getElementById("info");
-
-playBtn.href=PLAYSTORE;
-iosBtn.href=APPSTORE;
-
-}
-
+    playBtn.setAttribute("href", PLAYSTORE);
+    iosBtn.setAttribute("href", APPSTORE);
+});
